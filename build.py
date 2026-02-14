f8bda12aba319950aea2d3a8fb0095b77a6c95f6
Andrey F <sqin00@gmail.com>
2026-02-14 23:41:01 +0300

    Changed version

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ import locale
 
 APP_NAME = "FJ_Chat_Voice"
 MAIN_SCRIPT = "main.py"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 FILE_NAME = f"{APP_NAME}_{VERSION}"
 
 TORCH_CACHE_DIR = Path.home() / ".cache" / "torch" / "hub"
